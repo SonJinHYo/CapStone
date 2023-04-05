8b4fa5d7514ff3e861f666848d873a5c5b5988b2
SonJinHYo <wlsgy95@naver.com>
2023-04-05 10:58:50 +0900

    url 수정

--- violations/urls.py
+++ violations/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("service", views.AllViolations.as_view(), name="all_violations"),
     path("choice/<str:kind>", views.Choice.as_view(), name="choice"),
     path(
-        "service/<str:choice1>/<str:choice2>",
+        "choice/<str:choice1>/<str:choice2>",
         views.ViolationDetail.as_view(),
         name="violation_detail",
     ),
