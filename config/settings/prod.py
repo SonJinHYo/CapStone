a67a1a12c8a7c80d1275962cdf0e0b3d7d02347b
SonJinHYo <wlsgy95@naver.com>
2023-04-28 17:16:26 +0000

    AWS RDS와 연결

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,10 @@
 from . base import *
+import pymysql
+import environ
+
+pymysql.install_as_MySQLdb()
+pymysql.version_info = (1, 4, 13, "final", 0)
+env = environ.Env()
 
 ALLOWED_HOSTS = [
     "ec2-43-200-248-130.ap-northeast-2.compute.amazonaws.com",
@@ -7,9 +13,16 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # engine: mysql
+        'NAME' : 'mysqlDB', # DB Name
+        'USER' : 'admin', # DB User
+        'PASSWORD' : env("AWS_DB_PASSWORD"), # Password
+        'HOST': 'database-1.c3zb6mrofwf7.ap-northeast-2.rds.amazonaws.com', # 생성한 데이터베이스 엔드포인트
+        'PORT': '3306', # 데이터베이스 포트
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
