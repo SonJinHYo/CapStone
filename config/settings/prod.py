81f4ef4fe6ddef2c5a246260b88ebbee6f4e9a7e
SonJinHYo <wlsgy95@naver.com>
2023-04-28 13:11:49 +0000

    DEBUG 설정 및 탄력적 IP 할당

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,8 @@
 from . base import *
 
 ALLOWED_HOSTS = [
-    "ec2-13-125-148-138.ap-northeast-2.compute.amazonaws.com",
+    "ec2-43-200-248-130.ap-northeast-2.compute.amazonaws.com",
 ]
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = False
+DEBUG = True
